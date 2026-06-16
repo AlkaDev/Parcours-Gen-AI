b390fb6f3fc5339581cb5d2075d9d175953d0275
AlkaDev <musasakaleya@gmail.com>
2026-06-16 16:19:55 +0100

    ajout du fichier de code principal

---
+++ guess_the_number.py
@@ -0,0 +1,20 @@
+import random
+nombre_secret = random.randint(1, 150)
+print("Moi j'ai choisi le nombre entre 1 et 150. À toi de deviner !")
+gagne = False
+
+# la boucle while va nous aider à signaler à l'utilisateur que tant que :
+while not gagne:
+    saisir = input("tente avec ta proposition : ")
+    # et converti le texte en un nombre entier
+    essaye = int(saisir)
+    
+    if essaye < nombre_secret:
+        print("Trop petit")
+    elif essaye > nombre_secret:
+        print("Trop grand")
+    else:
+        print("Félicitation ! vous avez trouvé le nombre")
+        gagne = True
+
+
