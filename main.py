a73637c4db894f46535fe757c747cd885daad304
AlkaDev <musasakaleya@gmail.com>
2026-06-24 19:34:41 +0100

    création de la classe route

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("similation de route")
+
+canvas = tk.Canvas(root, width=900, height=600, bg="#056920")
+canvas.pack()
+
+class Route:  pass
+route = Route()
+
 root.mainloop()
 
