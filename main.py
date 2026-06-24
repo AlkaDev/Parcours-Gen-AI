296dbd01c239f514ba9cfffc6b4f5638d3346707
AlkaDev <musasakaleya@gmail.com>
2026-06-24 20:03:21 +0100

    Ajout de la classe Route et dessin de la route horizontale

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import tkinter as tk
 
+# Création de la fenêtre
 root = tk.Tk()
-root.title("similation de route")
 
-canvas = tk.Canvas(root, width=900, height=600, bg="#056920")
+root.title("Simulation trafic routier")
+
+# Création du canvas
+canvas = tk.Canvas(
+    root,
+    width=900,
+    height=600,
+    bg="#056920"
+)
+
 canvas.pack()
 
-class Route:  pass
+# Classe pour dessiner la route
+class Route:
+
+    def dessiner(self):
+
+        canvas.create_rectangle(
+            0,
+            200,
+            900,
+            400,
+            fill="#555555",
+            outline=""
+        )
+
+# Création de la route
 route = Route()
+
+# Dessin de la route
+route.dessiner()
+
+
 
 root.mainloop()
 
